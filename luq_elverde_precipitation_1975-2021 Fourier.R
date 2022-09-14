8a6a85797ff1a208c01f642b5da86d03a41f11f8
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2022-09-14 19:00:37 -0400

    data

--- luq_elverde_precipitation_1975-2021 Fourier.R
+++ luq_elverde_precipitation_1975-2021 Fourier.R
@@ -18,40 +18,116 @@ rm(list=ls(all=TRUE)) #give R a blank slate
 
 
 setwd("D:/Curriculum/14_ Colaboracion/2021 Pulse LTER/Pulse WG PR/PulseDynWorkingGroup_Luq/data")
-All_var_1975_2021=read.csv("All_variables 1975-2021.csv")
-head(All_var_1975_2021)
-summary(All_var_1975_2021)
+rain <- read.csv("evra1975-2021.csv")
+head(rain)
+summary(rain)
 
-maxTempLuq <- All_var_1975_2021 %>% select(date, max_temp, min_temp) %>% # select date, max and min
-  mutate(max_temp_apro = na.approx(max_temp)) %>% # extrapolate max
-  mutate(min_temp_apro = na.approx(min_temp)) # extrapolate min
-summary(maxTempLuq)
+rainLuq <- rain %>% select(date,rainfall) %>%
+  filter(!is.na(date)) %>%
+  filter(!is.na(rainfall))
 
+summary(rainLuq)
+head(rainLuq)
+tail(rainLuq)
 
+# Step 1 ------------------------------------------------------------------
 
-# Select only Maximum Temperature -----------------------------------------
-maxTempLuq_apro <- maxTempLuq %>% select(date, max_temp_apro) # let's work with max first
-summary(maxTempLuq_apro)
-
-
-#positivize the data for max_temp_apro by adding 20, cannot take log of zero/negative
-maxTempLuq_apro$max_temp_apro <- maxTempLuq_apro$max_temp_apro+20
+#positivize the data for rainfall by adding 20, cannot take log of zero/negative
+rainLuq$rainfall <- rainLuq$rainfall+20
 
 
-### date format ###
-maxTempLuq_apro$date <- as.Date(maxTempLuq_apro$date)
+# Step 2 ------------------------------------------------------------------
+
+rainLuq$date <-as.POSIXct(rainLuq$date,"%Y-%m-%d",tz = "UTC")
+summary(rainLuq)
+
+# Step 3 ------------------------------------------------------------------
 
 ### Fourier Analysis & Data Extremes ###
 #Create 'streamflow' object
-luq_Tmax.data<-asStreamflow(maxTempLuq_apro,start.date="1975-01-01",
-                             end.date="2021-12-31", river.name="El Verde")
+rainLuq. <- asStreamflow(rainLuq, start.date="1975-01-01",
+                        end.date="2021-03-30", 
+                        river.name="El Verde")
+
+
+# Step 4 ------------------------------------------------------------------
 
 ###Plot data to visually inspect time-series pulses
-luq_Tmax.data<-luq_Tmax.data$data
-plot(luq_Tmax.data$discharge ~ luq_Tmax.data$date, type="l", ylab="Maximum Air Temperature (C)", xlab="Date")
+rainLuq <- rainLuq$data
+plot(rainLuq$discharge~rainLuq$date, type="l", 
+     ylab="Precipitation (mm)", xlab="Date")
 
 
 
+# Step 5 ------------------------------------------------------------------
 
+#Fourier Analysis
+rainLuq.fourier <- fourierAnalysis(rainLuq.)
+summary(rainLuq.fourier) # SUMMARY STATS
+#1)"noise color" and is a measure of flashiness. Noise color ranges from 0 (white noise with no autocorrelation-highly flashy) to values
+#over 2 (reddened, values with strong autocorrelation in which high- or low- events persist over the short term).
+#2)"signal-noise ratio" is a measure of seasonality (higher is more seasonal).
+#3) "average" is long-term ordinal day average.
+plot(rainLuq.fourier, plot.type="hydrograph") # PLOT
+
+
+# Step 6 ------------------------------------------------------------------
+
+#Calculate annual extremes
+rainLuq.extremes<-annualExtremes(rainLuq.)
+names(rainLuq.extremes)
+
+
+# compare for periods from 2002 to 2011 and 2012 to 2021
+comp = compare.periods(c("2002-01-01", "2011-12-31"),
+                       c("2012-01-01", "2021-01-31"), rainLuq., plot=T)
+
+
+# Step 7. Export results --------------------------------------------------
+
+# allstats function works on files
+# write R 'streamflow' object data into csv file
+rainLuq. <- rainLuq.$data
+write.csv(rainLuq., "Rain Luq max.csv")
+
+RainLuq.allstats <- allstats(file.name = "evra1975-2021.csv", river.name = "El Verde", 
+                             file.type="csv", date.col=1,
+                             discharge.col=2, skipped.rows=0)
+
+#A data frame with columns
+#a.rms = Root mean squared amplitude.
+#n.rms = Root mean squared noise.
+#snr = Signal-to-noise ratio.
+#theta.d = Daily noise color.
+#theta.a = Annual noise color.
+#sigma.lf = Sigma for low flow events.
+#sigma.hf = Sigma for high flow events.
+#q2 = 2-year return level (flood).
+#q10 = 10-year return level (flood).
+#l2 = 2-year return level (drought).
+#l10 = 10-year return level (drought).
+
+
+
+### Long-term median values per Julian day ###
+median.year <- tapply(rainLuq.$discharge, list(rainLuq.$year), median)
+median.sum <- median(median.year) #40.415
+median.jday <- tapply(rainLuq$discharge, list(rainLuq$jday), median)
+jday <- tapply(rainLuq$jday, list(rainLuq$jday), mean)
+year <- tapply(rainLuq$year, list(rainLuq$year), mean)
+
+### Long-term min and max per year ###
+max.annual <- tapply(rainLuq$discharge, list(rainLuq$year), max)
+min.annual <- tapply(rainLuq$discharge, list(rainLuq$year), min)
+
+###Phenologies to assess mean date and CV
+###min<- lm(jday~year) ### slope and p-value could indicate pheno-shifts (pulse metric)
+###max<- lm(jday~year) ### slope and p-value could indicate pheno-shifts (pulse metric)
+###days.min and days.max could assess the pulse phenology and character
+###max.jday$annual.max - min.jday$annual.min
+min<-lm(min.annual~year)
+summary(min)
+min.slope<--0.06145
+min.p<-0.429
 
 
--- luq_precipitation_1975-2021 wavelet.R
+++ luq_precipitation_1975-2021 wavelet.R
@@ -3,7 +3,8 @@
 
 
 # ---------------------------------------------
-# Discharge Pool 0
+# Precipitation El Verde Field Station 1975-2021
+# wavelet analysis
 # 22 Jun 2021
 # Pablo E. Gutiérrez-Fonseca
 # pabloe.gutierrezfonseca@gmail.com
@@ -13,7 +14,6 @@
 
 
 rm(list=ls(all=TRUE)) #give R a blank slate
-
 
 
 
@@ -53,69 +53,4 @@ LUQ_walvelet <- wt.image(rain_wt, main = "Luquillo-LTER Daily Precipitation",
 wt.avg(rain_wt, siglvl = 0.01, sigcol = "red", sigpch = 20,
        periodlab = "period (days)")
 
-
-
-# Wavelet Discharge Pool 0 ------------------------------------------------
-
-allNA=read.csv("physicochemistry.csv")
-head(allNA)
-summary(allNA)
-
-dischargeNew <- dischargeNA %>% select(week,discharge) %>%
-  filter(!is.na(week)) %>%
-  filter(!is.na(discharge))
-
-
-dischargeNew$week <-as.POSIXct(dischargeNew$week,"%Y-%m-%d",tz = "UTC")
-
-discharge.wt <- analyze.wavelet(dischargeNew, "discharge",make.pval = TRUE, n.sim = 10) 
-
-wt.image(discharge.wt, main = "Luquillo-LTER weekly Discharge, Pool 0 (2000-2017)",
-                         periodlab = "period (week)",
-                         label.time.axis = T, show.date = T, date.format = "%Y-%m-%d",
-                         color.key = "quantile",legend.params = list(label.digits = 3, lab = "wavelet power levels", mar = 8),
-                         plot.contour = FALSE)   # without contour lines
-
-
-# Temperature -------------------------------------------------------------
-
-allNA=read.csv("physicochemistry.csv")
-head(allNA)
-summary(allNA)
-
-TemperatureNew <- allNA %>% select(week,Temperature) %>%
-  filter(!is.na(week)) %>%
-  filter(!is.na(Temperature))
-
-TemperatureNew$week <-as.POSIXct(TemperatureNew$week,"%Y-%m-%d",tz = "UTC")
-
-Temperature.wt <- analyze.wavelet(TemperatureNew, "Temperature", make.pval = TRUE, n.sim = 10) 
-
-wt.image(Temperature.wt, main = "Luquillo-LTER weekly water Temperature, Pool 0 (2000-2017)",
-         periodlab = "period (week)",
-         label.time.axis = T, show.date = T, date.format = "%Y-%m-%d",
-         color.key = "quantile",legend.params = list(label.digits = 3, lab = "wavelet power levels", mar = 8),
-         plot.contour = FALSE)   # without contour lines
-
-
-
-# DOC -------------------------------------------------------------
-
-allNA=read.csv("physicochemistry.csv")
-head(allNA)
-summary(allNA)
-
-DOCNew <- allNA %>% select(week,DOC) %>%
-  filter(!is.na(week)) %>%
-  filter(!is.na(DOC))
-
-DOCNew$week <-as.POSIXct(DOCNew$week,"%Y-%m-%d",tz = "UTC")
-
-DOC.wt <- analyze.wavelet(DOCNew, "DOC", make.pval = TRUE, n.sim = 10) 
-
-wt.image(DOC.wt, main = "Luquillo-LTER weekly DOC, Pool 0 (2000-2017)",
-         periodlab = "period (week)",
-         label.time.axis = T, show.date = T, date.format = "%Y-%m-%d",
-         color.key = "quantile",legend.params = list(label.digits = 3, lab = "wavelet power levels", mar = 8),
-         plot.contour = FALSE)   # without contour lines
 
